3c85ef8e2c1e0c2206eaf52ebe1aadea1b0e861c
icy3141 <icy3141@gmail.com>
2023-02-18 14:32:55 -0600

    installing require.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,42 @@
 ﻿
-//let form = document.forms.namedItem("uiForm");
-//let inputElem = form.elements["input1"];
+// jsdeliver CDN
+// https://www.jsdelivr.com/documentation#id-github
+// template:
+// https://cdn.jsdelivr.net/gh/user/repo@version/file
 
-//let promptElem = document.getElementById("prompt");
-//promptElem.innerText = ;
-
-
-
+const myCdnPath = "https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/";
 
 
 
+
+<script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/Recipe.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/CommandType.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/CommandData.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/socket.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/api.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/uiTools.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/uiInit.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/ui.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/icy3141/NutrientDoserClientApp/app/script.js" type="text/javascript"></script>
+
+const myJs = [
+    "Recipe",
+    "CommandType",
+    "CommandData",
+    "socket",
+    "api",
+    "uiTools",
+    "uiInit",
+    "ui"
+];
+
+requirejs(myJs, function (util) {
+    //This function is called when scripts/helper/util.js is loaded.
+    //If util.js calls define(), then this function is not fired until
+    //util's dependencies have loaded, and the util argument will hold
+    //the module value for "helper/util".
+    initialize();
+});
 
 
 function initialize() {
