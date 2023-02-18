58617cf0aa0babdd61396ab5061510cb7dd24d27
icy3141 <icy3141@gmail.com>
2023-02-18 16:36:30 -0600

    switch away from require.js to flat script loader

--- app/scripts/scriptLoader.js
+++ app/scripts/scriptLoader.js
@@ -28,18 +28,29 @@ for (let i = 0; i < myJs.length; i++) {
     myJs[i] += ".js";
 }
 
-require.config({
-    baseUrl: myCdnPath + "scripts/"
-    /*paths: {
-        "some": "some/v1.0"
-    },*/
-    /*waitSeconds: 15*/
-});
+let loadCounter = 0;
 
-requirejs(myJs, function (util) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-    initialize();
-});
+function addScript(fileName) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = myCdnPath + "scripts/" + fileName;
+    script.onload += () => {
+        loadCounter++;
+    }
+    document.head.appendChild(script);
+}
+
+function areAllScriptsLoaded() {
+    return loadCounter == myJs.length;
+}
+
+for (let i = 0; i < myJs.length; i++) {
+    addScript(myJs[i]);
+}
+
+const loaderTimer = setInterval(() => {
+    if (areAllScriptsLoaded()) {
+        clearInterval(loaderTimer);
+        initialize();
+    }
+}, 50);
